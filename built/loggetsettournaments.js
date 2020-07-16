8296fe279e8a4a9d05bcc56c9b10cbdc05b708cb
ymyropolets <yevhenii.m@moonactive.com>
2020-07-16 14:41:41 +0300

    Add additional log of get commands.

--- built/loggetsettournaments.js
+++ built/loggetsettournaments.js
@@ -1,4 +1,5 @@
 const expectedCommands = ['hset', 'hmset', 'hget', 'hmget'];
+const getCommands = ['hget', 'hset'];
 const expectedKeyRegex = /events:.*/;
 let currentLogger = null;
 function logSetGetTournamentsCommand(action, command, logger = currentLogger) {
@@ -10,6 +11,9 @@ function logSetGetTournamentsCommand(action, command, logger = currentLogger) {
         [key, field, payload] = args;
     if (field === 'tournaments' && expectedCommands.includes(name) && expectedKeyRegex.test(key)) {
         logger.warn('REDIS COMMAND LOG TOURNAMENTS ' + action, { name, key, field, payload });
+    }
+    if (getCommands.includes(name)) {
+        logger.warn('REDIS COMMAND LOG GET COMMAND ' + action, { name, key, field, payload, args });
     }
 }
 module.exports = {
